db2951764831252d9ae9132b9efaf2ae55a03ba3
Julia <jvhormuth@gmail.com>
2025-11-11 19:30:24 +0100

    create sigin up and dashboard

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,7 +1,37 @@
+import { useState } from "react";
+
+import "./sigin.css";
+import logo from "../../assets/logo.png";
+import { Link } from "react-router-dom";
+
 export default function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div>
-      <h1>Página login</h1>
+    <div className="container-center">
+      <div className="login">
+        <div className="login-area">
+          <img src={logo} alt="Basic System Logo" />
+        </div>
+        <form>
+          <h1>Entrar</h1>
+          <input
+            type="text"
+            placeholder="email@email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="*******"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit"> Acessar </button>
+        </form>
+        <Link to="register">Criar uma conta</Link>
+      </div>
     </div>
   );
 }
